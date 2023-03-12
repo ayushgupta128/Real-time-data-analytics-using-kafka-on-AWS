fa2df843a491d425c8b55c4cd7e3004717597352
Ayush Gupta <ayushgupta20721@gmail.com>
2023-03-13 01:36:57 +0530

    Adding code file for realtime project on AWS using kafka

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Real-time-data-analytics-using-kafka-on-AWS
+
+In this Project I Simulated a real time covid 19 data from https://api.covid19api.com/summary API.
+
+The data is filtered and cleaned using pandas and then sent to Kafka topic setup in AWS EC2.
+The data is read from the Kafka topic using Spark Streaming ReadStream API. It isaggregated by calculating the stats of the columns and pushed to different Kafka topic.
+
+The aggregated real time data is pushed to S3 using Boto3 SDK Library in Json format for furthur analysis.
+
+The data is crawled over using AWS Glue Crawler on S3 location and data is queried using AWS Athena for Data Analytics.
+
---
+++ covid19-consumer.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from kafka import KafkaProducer
+from time import sleep
+from json import loads
+import json
+from kafka import KafkaConsumer
+import boto3
+
+
+
+
+# Creating Consumer APIhui
+consumer = KafkaConsumer(
+    'demo-testing',
+     bootstrap_servers=['localhost:9092'], #add your IP here
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+#Uploading realtime data on S3
+s3=boto3.client('s3',aws_access_key_id=<access_key>,aws_secret_access_key=<secret_access_key>)
+for c in consumer:
+    df1=pd.DataFrame(loads(list(c.value.values())[0]),index=[0])
+    df1.to_csv('file1.csv',index=False)
+    message=c.value
+    s3.upload_file('file1.csv',Bucket='kafka-buckets',Key='confirmed-cases/day_id={0}/cases.csv'.format(list(c.value.keys())[0]))
+    print(c.value)    
