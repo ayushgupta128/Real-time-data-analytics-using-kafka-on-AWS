3c389c4e425f1d1695e83c66cd22ddbe393e1f9d
Ayush Gupta <ayushgupta20721@gmail.com>
2023-03-13 01:31:55 +0530

    initial commit

---
+++ covid_data.sql
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS covid19_table(
+`Global_new_confirmed` int,
+`Global_new_deaths` int,
+`Global_new_recovered` int,
+`Global_total_confirmed` int,
+`Global_total_deaths` int,
+`Global_total_recovered` int,
+`Country_code` string,
+`Country_name` string,
+`Country_new_deaths` int,
+`Country_new_recovered` int,
+`Country_newconfirmed` int,
+`Country_slug` string,
+`Country_total_confirmed` int,
+`Country_total_deaths` int,
+`Country_total_recovered` int,
+`Extracted_timestamp` timestamp)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+;
